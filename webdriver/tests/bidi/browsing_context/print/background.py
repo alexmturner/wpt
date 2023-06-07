ace5898389073d1550d1ec5152edf02612609b1e
Thiago Perrotta <tperrotta@chromium.org>
2023-06-07 17:53:33 +0200

    browsing_context/print: use dimensions larger than 1x1
    Chrome Print to PDF does not support tiny dimensions (c.f.
    https://crbug.com/1430696).
    
    The background test should not exercise dimensions testing. Instead, it
    should be concerned merely with the background property.

--- webdriver/tests/bidi/browsing_context/print/background.py
+++ webdriver/tests/bidi/browsing_context/print/background.py
@@ -16,16 +16,16 @@ INLINE_BACKGROUND_RENDERING_TEST_CONTENT = """
 </style>
 """
 
-BLACK_DOT_PNG = "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVQIW2NgYGD4DwABBAEAwS2OUAAAAABJRU5ErkJggg=="
-WHITE_DOT_PNG = "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAC0lEQVQIW2P4DwQACfsD/Z8fLAAAAAAASUVORK5CYII="
+PAGE_WITH_BLACK_DOT_PNG = "iVBORw0KGgoAAAANSUhEUgAAAGAAAABgCAYAAADimHc4AAAAAXNSR0IArs4c6QAAAbFJREFUeF7t1cFtAmEQxWC2ZRqhFjqgMjZKrlEuzCAT6eP+j7W2njjO8zwvfpmBQ4DM/Q9YgNa/ALF/AQSoDcR8/wECxAZivAUIEBuI8RYgQGwgxluAALGBGG8BAsQGYrwFCBAbiPEWIEBsIMZbgACxgRhvAQLEBmK8BQgQG4jxFiBAbCDGW4AAsYEYbwECxAZivAUIEBuI8RYgQGwgxluAAL8NPB6Py/1+f4ua2+32lruvHv3IBXxLul6vr37Tn++O47g8n8/1u5ODAkzsLbwVYEHi5IQAE3sLbwVYkDg5IcDE3sJbARYkTk4IMLG38FaABYmTEwJM7C28FWBB4uSEABN7C28FWJA4OSHAxN7CWwEWJE5OCDCxt/BWgAWJkxMCTOwtvBVgQeLkhAATewtvBViQODkhwMTewtuPDLDwXf/mhABxKgEEiA3EeAsQIDYQ4y1AgNhAjLcAAWIDMd4CBIgNxHgLECA2EOMtQIDYQIy3AAFiAzHeAgSIDcR4CxAgNhDjLUCA2ECMtwABYgMx3gIEiA3EeAsQIDYQ4y1AgNhAjLcAAWIDMd4CBIgNxPgveGwI/9dm758AAAAASUVORK5CYII="
+PAGE_WITH_WHITE_DOT_PNG = "iVBORw0KGgoAAAANSUhEUgAAAGAAAABgCAYAAADimHc4AAAAAXNSR0IArs4c6QAAAU1JREFUeF7t1cEJADAMw8B2/6Fd6BL3URYQSJjcbTsdM3ALwNx/cAGs/wJg/wUogDaA+f2AAmADGN8CCoANYHwLKAA2gPEtoADYAMa3gAJgAxjfAgqADWB8CygANoDxLaAA2ADGt4ACYAMY3wIKgA1gfAsoADaA8S2gANgAxreAAmADGN8CCoANYHwLKAA2gPEtoADYAMa3gAJgAxjfAgqADWB8CygANoDxLaAA2ADGt4ACYAMY3wIKgA1gfAsoADaA8S2gANgAxreAAmADGN8CCoANYHwLKAA2gPEtoADYAMa3gAJgAxjfAgqADWB8CygANoDxLaAA2ADGt4ACYAMY3wIKgA1gfAsoADaA8S2gANgAxreAAmADGN8CCoANYHwLKAA2gPEtoADYAMa3gAJgAxjfAgqADWB8CygANoDxLaAA2ADGt4ACYAMY/wDqB37wfELAlwAAAABJRU5ErkJggg=="
 
 
 @pytest.mark.parametrize(
     "print_with_background, expected_image",
     [
-        (None, WHITE_DOT_PNG),
-        (True, BLACK_DOT_PNG),
-        (False, WHITE_DOT_PNG),
+        (None, PAGE_WITH_WHITE_DOT_PNG),
+        (True, PAGE_WITH_BLACK_DOT_PNG),
+        (False, PAGE_WITH_WHITE_DOT_PNG),
     ],
 )
 async def test_background(
@@ -45,8 +45,16 @@ async def test_background(
     print_value = await bidi_session.browsing_context.print(
         context=top_context["context"],
         background=print_with_background,
-        margin={"top": 0, "bottom": 0, "right": 0, "left": 0},
-        page={"width": px_to_cm(1), "height": px_to_cm(1)},
+        margin={
+            "top": 0,
+            "bottom": 0,
+            "right": 0,
+            "left": 0
+        },
+        page={
+            "width": px_to_cm(96),
+            "height": px_to_cm(96),
+        },
     )
 
     assert_pdf(print_value)
